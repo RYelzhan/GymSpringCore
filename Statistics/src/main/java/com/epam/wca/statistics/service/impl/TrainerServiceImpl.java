da2e9ee3deb06a66019afa8411aedd3997c887ee
Yelzhan Rakhimzhanov <rakhimzhanov.yelzhan@gmail.com>
2024-12-20 02:02:18 +0500

    Refactored/Reduced Complexity of delete method in Statistics.TrainerServiceImpl. Updated tests of the service to be appropriate for current service.

--- Statistics/src/main/java/com/epam/wca/statistics/service/impl/TrainerServiceImpl.java
+++ Statistics/src/main/java/com/epam/wca/statistics/service/impl/TrainerServiceImpl.java
@@ -5,6 +5,7 @@ import com.epam.wca.common.gymcommon.statistics_dto.TrainerTrainingAddDTO;
 import com.epam.wca.common.gymcommon.statistics_dto.TrainerWorkloadSummary;
 import com.epam.wca.common.gymcommon.statistics_dto.TrainersTrainingsDeleteDTO;
 import com.epam.wca.statistics.exception.BadDataException;
+import com.epam.wca.statistics.exception.NoDataException;
 import com.epam.wca.statistics.repository.TrainerTrainingSummaryRepository;
 import com.epam.wca.statistics.service.TrainerService;
 import com.epam.wca.statistics.util.DTOFactory;
@@ -14,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.ZonedDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,11 +23,11 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 public class TrainerServiceImpl implements TrainerService {
-    public static final String TRAINING_DURATION_FOR_DELETION_CAN_NOT_BE_BIGGER_THAN_EXISTING_TRAINING_DURATION =
-            "Training duration for deletion can not be bigger than training duration of trainer on that day.";
-    public static final String NO_TRAINING_INFO_FOUND_FOR_TRAINER_ON_GIVEN_YEAR_AND_MONTH =
-            "No training info found for trainer \"%s\" on year \"%s\" and month \"%s\"";
-
+    public static final String SUMMARY_DURATION_LESS_THAN_SUBTRACTING = "Training summary duration is less than duration wanting to subtract." +
+            " Available duration: %s, Duration wanted to subtract: %s";
+    public static final String NO_TRAINER_SUMMARY_FOUND_FOR_TRAINER = "No Trainer Summary found for trainer: %s";
+    public static final String EMPTY_YEAR = "No Training summary available for year: %s";
+    public static final String NO_SUMMARY_FOR_YEAR_AND_MONTH = "No Training summary available for year %s and month %s";
     private final TrainerTrainingSummaryRepository trainingSummaryRepository;
 
     @Override
@@ -82,46 +84,67 @@ public class TrainerServiceImpl implements TrainerService {
     @Logging
     @Transactional
     public void deleteTrainings(TrainersTrainingsDeleteDTO trainingsDeleteDTO) {
-        trainingsDeleteDTO.getTrainingsInfo().forEach((username, trainerDetails) -> {
-            var trainerTrainingSummaryHolder =
-                    trainingSummaryRepository.findTrainerTrainingSummaryByUsername(username);
+        trainingsDeleteDTO.getTrainingsInfo().forEach(this::processTrainerDetails);
+    }
 
-            if (trainerTrainingSummaryHolder.isEmpty() && !trainerDetails.isEmpty()) {
-                throw new BadDataException("No Trainer Summary found for trainer: " + username);
-            }
+    private void processTrainerDetails(String username, Map<ZonedDateTime, Integer> trainerDetails) {
+        var trainerTrainingSummaryHolder =
+                trainingSummaryRepository.findTrainerTrainingSummaryByUsername(username);
 
-            if (trainerTrainingSummaryHolder.isEmpty()) {
-                return;
-            }
+        if (trainerTrainingSummaryHolder.isEmpty() && !trainerDetails.isEmpty()) {
+            throw new NoDataException(NO_TRAINER_SUMMARY_FOUND_FOR_TRAINER.formatted(username));
+        }
 
-            var trainerTrainingSummary = trainerTrainingSummaryHolder.get();
+        if (trainerTrainingSummaryHolder.isEmpty()) {
+            return;
+        }
 
-            Map<Integer, Map<Integer,Integer>> yearSummary = trainerTrainingSummary.getSummary();
+        var trainerTrainingSummary = trainerTrainingSummaryHolder.get();
+        Map<Integer, Map<Integer, Integer>> yearSummary = trainerTrainingSummary.getSummary();
 
-            trainerDetails.forEach((date, durationToSubtract) -> {
-                Map<Integer, Integer> monthSummary = yearSummary.get(date.getYear());
-                if (monthSummary == null) {
-                    throw new BadDataException("No Training summary available for year: " + date.getYear());
-                }
+        trainerDetails.forEach((date, durationToSubtract) ->
+                updateYearAndMonthSummary(yearSummary, date, durationToSubtract)
+        );
 
-                Integer availableDuration = monthSummary.get(date.getMonthValue());
-                if (availableDuration == null) {
-                    throw new BadDataException("No Training summary available for year %s and month %s"
-                            .formatted(date.getYear(), date.getMonthValue()));
-                }
+        trainingSummaryRepository.save(trainerTrainingSummary);
+    }
 
-                int leftDuration = availableDuration - durationToSubtract;
+    private void updateYearAndMonthSummary(
+            Map<Integer, Map<Integer, Integer>> yearSummary,
+            ZonedDateTime date,
+            int durationToSubtract
+    ) {
+        Map<Integer, Integer> monthSummary = yearSummary.get(date.getYear());
+        if (monthSummary == null) {
+            throw new NoDataException(EMPTY_YEAR.formatted(date.getYear()));
+        }
 
-                if (leftDuration < 0) {
-                    throw new BadDataException(("Training summary duration is less than duration wanting to subtract." +
-                            "Available duration: %s, Duration wanted to subtract: %s")
-                            .formatted(availableDuration, durationToSubtract));
-                }
+        Integer availableDuration = monthSummary.get(date.getMonthValue());
+        if (availableDuration == null) {
+            throw new NoDataException(NO_SUMMARY_FOR_YEAR_AND_MONTH
+                    .formatted(date.getYear(), date.getMonthValue()));
+        }
 
-                monthSummary.put(date.getMonthValue(), leftDuration);
-            });
+        adjustMonthSummary(monthSummary, date.getMonthValue(), availableDuration, durationToSubtract);
 
-            trainingSummaryRepository.save(trainerTrainingSummary);
-        });
+        if (monthSummary.isEmpty()) {
+            yearSummary.remove(date.getYear());
+        }
+    }
+
+    private void adjustMonthSummary(Map<Integer, Integer> monthSummary, int month,
+                                    int availableDuration, int durationToSubtract) {
+        int leftDuration = availableDuration - durationToSubtract;
+
+        if (leftDuration < 0) {
+            throw new BadDataException(SUMMARY_DURATION_LESS_THAN_SUBTRACTING
+                    .formatted(availableDuration, durationToSubtract));
+        }
+
+        if (leftDuration == 0) {
+            monthSummary.remove(month);
+        } else {
+            monthSummary.put(month, leftDuration);
+        }
     }
 }
--- Statistics/src/test/java/com/epam/wca/statistics/service/impl/TrainerServiceImplTest.java
+++ Statistics/src/test/java/com/epam/wca/statistics/service/impl/TrainerServiceImplTest.java
@@ -2,198 +2,310 @@ package com.epam.wca.statistics.service.impl;
 
 import com.epam.wca.common.gymcommon.statistics_dto.TrainerTrainingAddDTO;
 import com.epam.wca.common.gymcommon.statistics_dto.TrainersTrainingsDeleteDTO;
+import com.epam.wca.common.gymcommon.util.AppConstants;
+import com.epam.wca.statistics.entity.TrainerTrainingSummary;
 import com.epam.wca.statistics.exception.BadDataException;
 import com.epam.wca.statistics.exception.NoDataException;
+import com.epam.wca.statistics.repository.TrainerTrainingSummaryRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.List;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class TrainerServiceImplTest {
     @Mock
-    private TrainerWorkloadRepository trainerWorkloadRepository;
+    private TrainerTrainingSummaryRepository trainingSummaryRepository;
     @InjectMocks
     private TrainerServiceImpl trainerService;
 
     @Test
     void addNewTraining_ShouldAddNewTraining_WhenTrainerWorkloadExists() {
         // Arrange
+        final int YEAR = 2024;
+        final int MONTH = 12;
+        final int DURATION_EXISTING = 10;
+        final int DURATION_TO_ADD = 20;
+
         TrainerTrainingAddDTO trainingAddDTO = new TrainerTrainingAddDTO(
                 "ethan.white",
                 "Ethan",
                 "White",
                 true,
-                ZonedDateTime.now(),
-                20
+                ZonedDateTime.parse("01.%s.%s 00:00:00 ".formatted(MONTH, YEAR) + ZoneId.systemDefault(),
+                        DateTimeFormatter.ofPattern(AppConstants.DEFAULT_DATE_FORMAT)),
+                DURATION_TO_ADD
         );
 
-        TrainerWorkload existingWorkload = new TrainerWorkload(
+        Map<Integer, Integer> monthSummary = new HashMap<>();
+        monthSummary.put(MONTH, DURATION_EXISTING);
+        Map<Integer, Map<Integer, Integer>> yearSummary = new HashMap<>();
+        yearSummary.put(YEAR, monthSummary);
+
+        TrainerTrainingSummary existingWorkload = new TrainerTrainingSummary(
                 "ethan.white",
                 "ethan",
                 "white",
                 true,
-                2024, 12, 0
+                yearSummary
         );
 
-        when(trainerWorkloadRepository.findByUsernameAndYearAndMonth(
-                trainingAddDTO.username(),
-                trainingAddDTO.date().getYear(),
-                trainingAddDTO.date().getMonthValue()
+        when(trainingSummaryRepository.findTrainerTrainingSummaryByUsername(
+                trainingAddDTO.username()
         )).thenReturn(Optional.of(existingWorkload));
 
         // Act
         trainerService.addNewTraining(trainingAddDTO);
 
         // Assert
-        Mockito.verify(trainerWorkloadRepository).save(Mockito.argThat(
-                workload -> workload.getDuration() == 20
-        ));
+        Mockito.verify(trainingSummaryRepository).save(
+                Mockito.argThat(
+                        workload ->
+                                workload.getSummary().get(YEAR).get(MONTH) == DURATION_EXISTING + DURATION_TO_ADD
+                )
+        );
     }
 
     @Test
     void addNewTraining_ShouldCreateNewTrainingWorkload_WhenTrainerWorkloadDoesNotExist() {
         // Arrange
+        final int YEAR = 2024;
+        final int MONTH = 12;
+        final int DURATION_TO_ADD = 20;
+
         TrainerTrainingAddDTO trainingAddDTO = new TrainerTrainingAddDTO(
                 "ethan.white",
                 "Ethan",
                 "White",
                 true,
-                ZonedDateTime.now(),
-                20
+                ZonedDateTime.parse("01.%s.%s 00:00:00 ".formatted(MONTH, YEAR) + ZoneId.systemDefault(),
+                        DateTimeFormatter.ofPattern(AppConstants.DEFAULT_DATE_FORMAT)),
+                DURATION_TO_ADD
         );
 
-        when(trainerWorkloadRepository.findByUsernameAndYearAndMonth(
-                trainingAddDTO.username(),
-                trainingAddDTO.date().getYear(),
-                trainingAddDTO.date().getMonthValue()
+        when(trainingSummaryRepository.findTrainerTrainingSummaryByUsername(
+                trainingAddDTO.username()
         )).thenReturn(Optional.empty());
 
         // Act
         trainerService.addNewTraining(trainingAddDTO);
 
         // Assert
-        Mockito.verify(trainerWorkloadRepository).save(Mockito.any(TrainerWorkload.class));
+        ArgumentCaptor<TrainerTrainingSummary> captor = ArgumentCaptor.forClass(TrainerTrainingSummary.class);
+        verify(trainingSummaryRepository).insert(captor.capture());
+
+        TrainerTrainingSummary actualSummary = captor.getValue();
+        assertNotNull(actualSummary);
+        assertEquals(DURATION_TO_ADD, actualSummary.getSummary().get(YEAR).get(MONTH));
     }
 
     @Test
-    void getWorkload_ShouldReturnWorkloadSummary_ForExistingTrainer() {
+    void getWorkload_ShouldReturnWorkloadSummary_WithExistingTrainings() {
         // Arrange
-        List<TrainerWorkload> workloads = List.of(
-                new TrainerWorkload(
-                        "ethan.white",
-                        "ethan",
-                        "white",
-                        true,
-                        2024, 12, 5),
-                new TrainerWorkload(
-                        "ethan.white",
-                        "ethan",
-                        "white",
-                        true,
-                        2025, 11, 3)
+        final int YEAR_1 = 2024;
+        final int MONTH_1 = 12;
+        final int DURATION_EXISTING_1 = 10;
+        final int YEAR_2 = 2025;
+        final int MONTH_2 = 10;
+        final int DURATION_EXISTING_2 = 15;
+        final int COUNT_OF_SUMMARIES = 2;
+
+        final String USERNAME = "ethan.white";
+
+        Map<Integer, Integer> monthSummary1 = new HashMap<>();
+        monthSummary1.put(MONTH_1, DURATION_EXISTING_1);
+        Map<Integer, Map<Integer, Integer>> yearSummary = new HashMap<>();
+        yearSummary.put(YEAR_1, monthSummary1);
+
+        Map<Integer, Integer> monthSummary2 = new HashMap<>();
+        monthSummary2.put(MONTH_2, DURATION_EXISTING_2);
+        yearSummary.put(YEAR_2, monthSummary2);
+
+        var existingWorkload = new TrainerTrainingSummary(
+                USERNAME,
+                "ethan",
+                "white",
+                true,
+                yearSummary
         );
-        when(trainerWorkloadRepository.findAllByUsername("ethan.white")).thenReturn(workloads);
+
+        when(trainingSummaryRepository.findTrainerTrainingSummaryByUsername(USERNAME))
+                .thenReturn(Optional.of(existingWorkload));
 
         // Act
-        var summary = trainerService.getWorkload("ethan.white");
+        var summary = trainerService.getWorkload(USERNAME);
 
         // Assert
-        Assertions.assertNotNull(summary);
-        Assertions.assertEquals(2, summary.trainingSummary().size());
+        assertNotNull(summary);
+        assertEquals(COUNT_OF_SUMMARIES, summary.trainingSummary().size());
+        assertEquals(DURATION_EXISTING_1, summary.trainingSummary().get(YEAR_1).get(MONTH_1));
+        assertEquals(DURATION_EXISTING_2, summary.trainingSummary().get(YEAR_2).get(MONTH_2));
+    }
+
+    @Test
+    void getWorkload_ShouldReturnWorkloadSummary_WithoutExistingTrainings() {
+        // Arrange
+        final String USERNAME = "ethan.white";
+
+        when(trainingSummaryRepository.findTrainerTrainingSummaryByUsername(USERNAME))
+                .thenReturn(Optional.empty());
+
+        // Act
+        var summary = trainerService.getWorkload(USERNAME);
+
+        // Assert
+        assertNotNull(summary);
+        assertTrue(summary.trainingSummary().isEmpty());
     }
 
     @Test
     void deleteTrainings_ShouldDeleteTraining_WhenDurationMatchesExactly() {
         // Arrange
-        TrainersTrainingsDeleteDTO deleteDTO = new TrainersTrainingsDeleteDTO(Map.of(
-                "ethan.white", Map.of(ZonedDateTime.now(), 5)
+        final String USERNAME = "ethan.white";
+
+        final int YEAR = 2024;
+        final int MONTH = 12;
+        final int DURATION_EXISTING = 5;
+        final int DURATION_TO_SUBTRACT = 5;
+
+        var deleteDTO = new TrainersTrainingsDeleteDTO(Map.of(
+                USERNAME, Map.of(
+                        ZonedDateTime.parse("01.%s.%s 00:00:00 ".formatted(MONTH, YEAR) + ZoneId.systemDefault(),
+                                DateTimeFormatter.ofPattern(AppConstants.DEFAULT_DATE_FORMAT)),
+                        DURATION_TO_SUBTRACT
+                )
         ));
-        TrainerWorkload workload = new TrainerWorkload(
-                        1L,
-                        "ethan.white",
+
+        Map<Integer, Integer> monthSummary = new HashMap<>();
+        monthSummary.put(MONTH, DURATION_EXISTING);
+        Map<Integer, Map<Integer, Integer>> yearSummary = new HashMap<>();
+        yearSummary.put(YEAR, monthSummary);
+
+        var workload = new TrainerTrainingSummary(
+                        String.valueOf(1L),
+                        USERNAME,
                         "ethan",
                         "white",
                         true,
-                        2024, 12, 5
+                        yearSummary
         );
 
-        when(trainerWorkloadRepository.findByUsernameAndYearAndMonth(
-                "ethan.white", 2024, 12
+        when(trainingSummaryRepository.findTrainerTrainingSummaryByUsername(
+                USERNAME
         )).thenReturn(Optional.of(workload));
 
         // Act
         trainerService.deleteTrainings(deleteDTO);
 
         // Assert
-        verify(trainerWorkloadRepository).deleteById(1L);
+        verify(trainingSummaryRepository).save(workload);
+        assertNull(workload.getSummary().get(YEAR).get(MONTH));
     }
 
     @Test
     void deleteTrainings_ShouldReduceTrainingDuration_WhenDurationIsLessThanExisting() {
         // Arrange
+        final String USERNAME = "ethan.white";
+
+        final int YEAR = 2024;
+        final int MONTH = 12;
+        final int DURATION_EXISTING = 5;
+        final int DURATION_TO_SUBTRACT = 3;
+
         TrainersTrainingsDeleteDTO deleteDTO = new TrainersTrainingsDeleteDTO(Map.of(
-                "ethan.white", Map.of(ZonedDateTime.now(), 3)
+                USERNAME, Map.of(
+                        ZonedDateTime.parse("01.%s.%s 00:00:00 ".formatted(MONTH, YEAR) + ZoneId.systemDefault(),
+                                DateTimeFormatter.ofPattern(AppConstants.DEFAULT_DATE_FORMAT)),
+                        DURATION_TO_SUBTRACT
+                )
         ));
 
-        TrainerWorkload workload = new TrainerWorkload(
-                "ethan.white",
+        Map<Integer, Integer> monthSummary = new HashMap<>();
+        monthSummary.put(MONTH, DURATION_EXISTING);
+        Map<Integer, Map<Integer, Integer>> yearSummary = new HashMap<>();
+        yearSummary.put(YEAR, monthSummary);
+
+        var workload = new TrainerTrainingSummary(
+                USERNAME,
                 "ethan",
                 "white",
                 true,
-                2024, 12, 5
+                yearSummary
         );
 
-        when(trainerWorkloadRepository.findByUsernameAndYearAndMonth(
-                "ethan.white", 2024, 12
+        when(trainingSummaryRepository.findTrainerTrainingSummaryByUsername(
+                USERNAME
         )).thenReturn(Optional.of(workload));
 
         // Act
         trainerService.deleteTrainings(deleteDTO);
 
         // Assert
-        Mockito.verify(trainerWorkloadRepository).save(Mockito.argThat(
-                w -> w.getDuration() == 2
+        Mockito.verify(trainingSummaryRepository).save(Mockito.argThat(
+                w -> w.getSummary().get(YEAR).get(MONTH).equals(DURATION_EXISTING - DURATION_TO_SUBTRACT)
         ));
     }
 
     @Test
     void deleteTrainings_ShouldThrowException_WhenDurationExceedsExisting() {
         // Arrange
+        final String USERNAME = "ethan.white";
+
+        final int YEAR = 2024;
+        final int MONTH = 12;
+        final int DURATION_EXISTING = 5;
+        final int DURATION_TO_SUBTRACT = 10;
+
         TrainersTrainingsDeleteDTO deleteDTO = new TrainersTrainingsDeleteDTO(Map.of(
-                "trainer1", Map.of(ZonedDateTime.now(), 10)
+                USERNAME, Map.of(
+                        ZonedDateTime.parse("01.%s.%s 00:00:00 ".formatted(MONTH, YEAR) + ZoneId.systemDefault(),
+                                DateTimeFormatter.ofPattern(AppConstants.DEFAULT_DATE_FORMAT)),
+                        DURATION_TO_SUBTRACT
+                )
         ));
 
-        TrainerWorkload workload = new TrainerWorkload(
-                "ethan.white",
+        Map<Integer, Integer> monthSummary = new HashMap<>();
+        monthSummary.put(MONTH, DURATION_EXISTING);
+        Map<Integer, Map<Integer, Integer>> yearSummary = new HashMap<>();
+        yearSummary.put(YEAR, monthSummary);
+
+        TrainerTrainingSummary workload = new TrainerTrainingSummary(
+                USERNAME,
                 "ethan",
                 "white",
                 true,
-                2024, 12, 5
+                yearSummary
         );
 
-        when(trainerWorkloadRepository.findByUsernameAndYearAndMonth(
-                "trainer1", 2024, 12
+        when(trainingSummaryRepository.findTrainerTrainingSummaryByUsername(
+                USERNAME
         )).thenReturn(Optional.of(workload));
 
         // Act & Assert
         Exception exception = Assertions.assertThrows(BadDataException.class, () -> {
             trainerService.deleteTrainings(deleteDTO);
         });
-        Assertions.assertEquals(
+        assertEquals(
                 TrainerServiceImpl.
-                        TRAINING_DURATION_FOR_DELETION_CAN_NOT_BE_BIGGER_THAN_EXISTING_TRAINING_DURATION,
+                        SUMMARY_DURATION_LESS_THAN_SUBTRACTING.formatted(DURATION_EXISTING, DURATION_TO_SUBTRACT),
                 exception.getMessage()
         );
     }
@@ -201,18 +313,117 @@ class TrainerServiceImplTest {
     @Test
     void deleteTrainings_ShouldThrowException_WhenNoTrainingFound() {
         // Arrange
+        final String USERNAME = "ethan.white";
+
+        final int YEAR = 2024;
+        final int MONTH = 12;
+        final int DURATION_TO_SUBTRACT = 5;
+
         TrainersTrainingsDeleteDTO deleteDTO = new TrainersTrainingsDeleteDTO(Map.of(
-                "trainer1", Map.of(ZonedDateTime.now(), 5)
+                USERNAME, Map.of(
+                        ZonedDateTime.parse("01.%s.%s 00:00:00 ".formatted(MONTH, YEAR) + ZoneId.systemDefault(),
+                                DateTimeFormatter.ofPattern(AppConstants.DEFAULT_DATE_FORMAT)),
+                        DURATION_TO_SUBTRACT
+                )
         ));
 
-        when(trainerWorkloadRepository.findByUsernameAndYearAndMonth(
-                "trainer1", 2024, 12
+        when(trainingSummaryRepository.findTrainerTrainingSummaryByUsername(
+                USERNAME
         )).thenReturn(Optional.empty());
 
         // Act & Assert
         Exception exception = Assertions.assertThrows(NoDataException.class, () -> {
             trainerService.deleteTrainings(deleteDTO);
         });
-        Assertions.assertTrue(exception.getMessage().contains("No training info found for trainer"));
+        assertEquals(
+                TrainerServiceImpl.NO_TRAINER_SUMMARY_FOUND_FOR_TRAINER.formatted(USERNAME),
+                exception.getMessage()
+        );
+    }
+
+    @Test
+    void deleteTrainings_ShouldThrowException_WhenNoSummaryForYearFound() {
+        // Arrange
+        final String USERNAME = "ethan.white";
+
+        final int YEAR = 2024;
+        final int MONTH = 12;
+        final int DURATION_TO_SUBTRACT = 10;
+
+        TrainersTrainingsDeleteDTO deleteDTO = new TrainersTrainingsDeleteDTO(Map.of(
+                USERNAME, Map.of(
+                        ZonedDateTime.parse("01.%s.%s 00:00:00 ".formatted(MONTH, YEAR) + ZoneId.systemDefault(),
+                                DateTimeFormatter.ofPattern(AppConstants.DEFAULT_DATE_FORMAT)),
+                        DURATION_TO_SUBTRACT
+                )
+        ));
+
+        Map<Integer, Map<Integer, Integer>> yearSummary = new HashMap<>();
+
+        TrainerTrainingSummary workload = new TrainerTrainingSummary(
+                USERNAME,
+                "ethan",
+                "white",
+                true,
+                yearSummary
+        );
+
+        when(trainingSummaryRepository.findTrainerTrainingSummaryByUsername(
+                USERNAME
+        )).thenReturn(Optional.of(workload));
+
+        // Act & Assert
+        Exception exception = Assertions.assertThrows(NoDataException.class, () -> {
+            trainerService.deleteTrainings(deleteDTO);
+        });
+        assertEquals(
+                TrainerServiceImpl.
+                        EMPTY_YEAR.formatted(YEAR),
+                exception.getMessage()
+        );
+    }
+
+    @Test
+    void deleteTrainings_ShouldThrowException_WhenNoSummaryForMonthFound() {
+        // Arrange
+        final String USERNAME = "ethan.white";
+
+        final int YEAR = 2024;
+        final int MONTH = 12;
+        final int DURATION_TO_SUBTRACT = 10;
+
+        TrainersTrainingsDeleteDTO deleteDTO = new TrainersTrainingsDeleteDTO(Map.of(
+                USERNAME, Map.of(
+                        ZonedDateTime.parse("01.%s.%s 00:00:00 ".formatted(MONTH, YEAR) + ZoneId.systemDefault(),
+                                DateTimeFormatter.ofPattern(AppConstants.DEFAULT_DATE_FORMAT)),
+                        DURATION_TO_SUBTRACT
+                )
+        ));
+
+        Map<Integer, Integer> monthSummary = new HashMap<>();
+        Map<Integer, Map<Integer, Integer>> yearSummary = new HashMap<>();
+        yearSummary.put(YEAR, monthSummary);
+
+        TrainerTrainingSummary workload = new TrainerTrainingSummary(
+                USERNAME,
+                "ethan",
+                "white",
+                true,
+                yearSummary
+        );
+
+        when(trainingSummaryRepository.findTrainerTrainingSummaryByUsername(
+                USERNAME
+        )).thenReturn(Optional.of(workload));
+
+        // Act & Assert
+        Exception exception = Assertions.assertThrows(NoDataException.class, () -> {
+            trainerService.deleteTrainings(deleteDTO);
+        });
+        assertEquals(
+                TrainerServiceImpl.
+                        NO_SUMMARY_FOR_YEAR_AND_MONTH.formatted(YEAR, MONTH),
+                exception.getMessage()
+        );
     }
 }
